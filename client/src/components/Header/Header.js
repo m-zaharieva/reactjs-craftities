a384516378556220b43253ae0cdf2d3df9b13cac
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-11-19 13:06:39 +0200

    edit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -44,7 +44,7 @@ function Header() {
 			<div className="container">
 				<div className="page-banner">
 					<div className="background-image">
-						<img src="/img/layers-colored-papers.jpg" alt="Ad banner" />
+						<img src="/img/explosion-colored-powder-white-background.jpg" alt="Ad banner" />
 					</div>
 					<div className="banner-title">
 						<h1>Make your art a life story. Make yourself a winner</h1>
