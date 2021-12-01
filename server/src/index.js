0a91047ca6045f7ece911640b8c2d2d888b03c23
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-12-01 21:35:12 +0200

    edit

--- server/src/index.js
+++ server/src/index.js
@@ -9,23 +9,25 @@ import { DB_CONNECTION_STRING } from './config/constants.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve('./src/public')))
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.use(router);
 
-app.use('/api/*', (req, res) => {
-    res.status(404)
-            .send(JSON.stringify(`Endpoint ${req.method.toUpperCase()} ${req.baseUrl}${req.path.slice(0, -1)} NOT FOUND!`));
-})
+// app.use('/api/*', (req, res) => {
+//     res.status(404)
+//             .send(JSON.stringify(`Endpoint ${req.method.toUpperCase()} ${req.baseUrl}${req.path.slice(0, -1)} NOT FOUND!`));
+// })
 
-app.use('*', (req, res) => {
-    res.sendFile('./../public/index.html');
-})
+// app.use('*', (req, res) => {
+//     res.sendFile('./../public/index.html');
+// })
 
 
 db(DB_CONNECTION_STRING)
